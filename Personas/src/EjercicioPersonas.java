41d031afa99d3c97b13d987b41f5b864c476c468
Gabriel Arroyo Pajares <gabrielarroyo396@gmail.com>
2017-11-20 10:24:40 +0100

    Personas

--- Personas/src/EjercicioPersonas.java
+++ Personas/src/EjercicioPersonas.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.Scanner;
 
+
+
+
+
 public class EjercicioPersonas {
 
 	static void EscribirPersonas(Persona [] gente) {
@@ -17,9 +21,9 @@ public class EjercicioPersonas {
 			
 			ficherobuffer.write(gente[i].getNombre());
 			ficherobuffer.newLine();
-			ficherobuffer.write(gente[i].getEdad());
-			ficherobuffer.newLine();
 			ficherobuffer.write(gente[i].getApellido());
+			ficherobuffer.newLine();
+			ficherobuffer.write(String.valueOf(gente[i].getEdad()));
 			ficherobuffer.newLine();
 		}
 		
@@ -32,11 +36,46 @@ public class EjercicioPersonas {
 	}
 	
 	
-	
-	
 	}
-	
-	
+	static void LeerPersonas(Persona[] lista) {
+		
+		
+		try {
+			
+			File fichero = new File("Personas.txt");
+			FileReader ficheroEscritura = new FileReader(fichero);
+			BufferedReader ficherobuffer = new BufferedReader(ficheroEscritura);
+			
+			String linea;
+			int i=0;
+			
+			linea=ficherobuffer.readLine();
+			while(linea != null) {
+				
+				lista[i].setNombre(linea);
+				linea=ficherobuffer.readLine();
+				lista[i].setApellido(linea);
+				linea=ficherobuffer.readLine();
+				lista[i].setEdad(Integer.parseInt(linea));
+				i++;
+				linea=ficherobuffer.readLine();
+			}
+					
+			
+			
+			
+			ficherobuffer.close();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		}
 	
 	
 	static void Vaciar(Persona [] gente) {
@@ -48,38 +87,52 @@ public class EjercicioPersonas {
 		}
 	}
 	
+	static void Escribir(Persona[] gente) {
+		
+		for (int i= 0; i < gente.length; i++) {
+		System.out.println("Nombre: " + gente[i].getNombre());	
+		System.out.println("Apellido: " + gente[i].getApellido());
+		System.out.println("Edad: " + gente[i].getEdad());
+		
+		}
+		
+	}
 	
 	static int Menu() {
-		Scanner scan = new Scanner(System.in);
-		int num = 0;
-		
-		
 		System.out.println("1º Escribir Fichero");
 		System.out.println("2º Leer Fichero");
 		System.out.println("3º Escribir Pantalla");
 		System.out.println("4º Vaciar Estructura");
 		System.out.println("5º Salir");
-		num = Integer.parseInt(scan.nextLine());
+		Scanner scan = new Scanner(System.in);
+		int numero = scan.nextInt();
 		scan.reset();
 		scan.close();
-		return num;
+		return numero;
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num = 0;
+		int num ;
 		Persona [] gente = {new Persona("Pepe","magneto",15), new Persona("Juan","Francisco",20),new Persona("Fernando","Silva",21),
 							new Persona("Alejandro","sanz",77),new Persona("Gabriel","Arroyo",50)};
 	
+		
 		do {
 		num = Menu();
 		switch (num) {
 		case 1:
 			EscribirPersonas(gente);
 			break;
-
-		default:
+		case 2:
+			LeerPersonas(gente);
+			break;
+		case 3:
+			Escribir(gente);
+			break;
+		case 4:
+			Vaciar(gente);
 			break;
 		}
 		}while(num != 5);
